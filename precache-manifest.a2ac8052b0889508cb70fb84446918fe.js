3e7426afb85b6a15777fac5598e9e1a9462faab6
TyroniUA <v.rudovtsymbalist@gmail.com>
2020-01-20 11:43:25 +0100

    Updates

---
+++ precache-manifest.a2ac8052b0889508cb70fb84446918fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab0964a099a14035de2bb7a8b3ac480",
+    "url": "/DrumMachine/index.html"
+  },
+  {
+    "revision": "596c62242479860c1e4a",
+    "url": "/DrumMachine/static/css/main.f7042014.chunk.css"
+  },
+  {
+    "revision": "71c8149a4c344402c3cd",
+    "url": "/DrumMachine/static/js/2.efcfc1fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/DrumMachine/static/js/2.efcfc1fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "596c62242479860c1e4a",
+    "url": "/DrumMachine/static/js/main.ad81901e.chunk.js"
+  },
+  {
+    "revision": "1b997279d54daa48c10d",
+    "url": "/DrumMachine/static/js/runtime-main.8c935c80.js"
+  }
+]);
