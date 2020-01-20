e6a69644df348a317233c291568e60121b3788e6
TyroniUA <v.rudovtsymbalist@gmail.com>
2020-01-20 11:44:23 +0100

    Updates

---
+++ precache-manifest.3c01cdd00bf92384f1a465456c63aebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb05a76d5f0ad7385f38a1a3e9b8e2fb",
+    "url": "/Drumpad/index.html"
+  },
+  {
+    "revision": "d6e134af99bb20119608",
+    "url": "/Drumpad/static/css/main.f7042014.chunk.css"
+  },
+  {
+    "revision": "71c8149a4c344402c3cd",
+    "url": "/Drumpad/static/js/2.efcfc1fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Drumpad/static/js/2.efcfc1fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6e134af99bb20119608",
+    "url": "/Drumpad/static/js/main.bdd3d74d.chunk.js"
+  },
+  {
+    "revision": "6d03520d71aa646a2a81",
+    "url": "/Drumpad/static/js/runtime-main.7e49beba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/DrumMachine/precache-manifest.a2ac8052b0889508cb70fb84446918fe.js"
+  "/Drumpad/precache-manifest.3c01cdd00bf92384f1a465456c63aebf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DrumMachine/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Drumpad/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
